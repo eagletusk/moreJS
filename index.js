0a7a8a02d6f9381dd65aaaab5ef020a98d43dd12
Paul Subzak <sobczak.paul@gmail.com>
2020-04-18 19:33:35 +0000

    Transpose Two Strings

--- index.js
+++ index.js
@@ -1,63 +1,101 @@
 
-//"\n".charCodeAt(0);
+// //"\n".charCodeAt(0);
 
-//A = 65 Z = 90
-//a = 97 Z = 122
-let a ={
-"31": "",      "32": " ",     "33": "!",     "34": "\"",    "35": "#",    
-"36": "$",     "37": "%",     "38": "&",     "39": "'",     "40": "(",    
-"41": ")",     "42": "*",     "43": "+",     "44": ",",     "45": "-",    
-"46": ".",     "47": "/",     "48": "0",     "49": "1",     "50": "2",    
-"51": "3",     "52": "4",     "53": "5",     "54": "6",     "55": "7",    
-"56": "8",     "57": "9",     "58": ":",     "59": ";",     "60": "<",    
-"61": "=",     "62": ">",     "63": "?",     "64": "@",     "65": "A",    
-"66": "B",     "67": "C",     "68": "D",     "69": "E",     "70": "F",    
-"71": "G",     "72": "H",     "73": "I",     "74": "J",     "75": "K",    
-"76": "L",     "77": "M",     "78": "N",     "79": "O",     "80": "P",    
-"81": "Q",     "82": "R",     "83": "S",     "84": "T",     "85": "U",    
-"86": "V",     "87": "W",     "88": "X",     "89": "Y",     "90": "Z",    
-"91": "[",     "92": "\\",    "93": "]",     "94": "^",     "95": "_",    
-"96": "`",     "97": "a",     "98": "b",     "99": "c",     "100": "d",    
-"101": "e",    "102": "f",    "103": "g",    "104": "h",    "105": "i",    
-"106": "j",    "107": "k",    "108": "l",    "109": "m",    "110": "n",    
-"111": "o",    "112": "p",    "113": "q",    "114": "r",    "115": "s",    
-"116": "t",    "117": "u",    "118": "v",    "119": "w",    "120": "x",    
-"121": "y",    "122": "z",    "123": "{",    "124": "|",    "125": "}",    
-"126": "~",    "127": ""
-};
+// //A = 65 Z = 90
+// //a = 97 Z = 122
+// let a ={
+// "31": "",      "32": " ",     "33": "!",     "34": "\"",    "35": "#",    
+// "36": "$",     "37": "%",     "38": "&",     "39": "'",     "40": "(",    
+// "41": ")",     "42": "*",     "43": "+",     "44": ",",     "45": "-",    
+// "46": ".",     "47": "/",     "48": "0",     "49": "1",     "50": "2",    
+// "51": "3",     "52": "4",     "53": "5",     "54": "6",     "55": "7",    
+// "56": "8",     "57": "9",     "58": ":",     "59": ";",     "60": "<",    
+// "61": "=",     "62": ">",     "63": "?",     "64": "@",     "65": "A",    
+// "66": "B",     "67": "C",     "68": "D",     "69": "E",     "70": "F",    
+// "71": "G",     "72": "H",     "73": "I",     "74": "J",     "75": "K",    
+// "76": "L",     "77": "M",     "78": "N",     "79": "O",     "80": "P",    
+// "81": "Q",     "82": "R",     "83": "S",     "84": "T",     "85": "U",    
+// "86": "V",     "87": "W",     "88": "X",     "89": "Y",     "90": "Z",    
+// "91": "[",     "92": "\\",    "93": "]",     "94": "^",     "95": "_",    
+// "96": "`",     "97": "a",     "98": "b",     "99": "c",     "100": "d",    
+// "101": "e",    "102": "f",    "103": "g",    "104": "h",    "105": "i",    
+// "106": "j",    "107": "k",    "108": "l",    "109": "m",    "110": "n",    
+// "111": "o",    "112": "p",    "113": "q",    "114": "r",    "115": "s",    
+// "116": "t",    "117": "u",    "118": "v",    "119": "w",    "120": "x",    
+// "121": "y",    "122": "z",    "123": "{",    "124": "|",    "125": "}",    
+// "126": "~",    "127": ""
+// };
 
-function flipPairs(inputs) {
-  let a = 0;
+// function flipPairs(inputs) {
+//   let a = 0;
+//   let result = [];
+//   for (let i= 0; i<inputs.length;i++){
+//     let value = inputs[i].charCodeAt(0);
+//     //UPPER CASE
+//     if(value >= 65 && value <= 90){
+//       if(value <= 77) {
+//         result[i] = String.fromCharCode(value+13); 
+//       }
+//       if (value > 77){
+//          result[i] = String.fromCharCode(value -13); 
+//       }
+//     }
+//     //LOWER CASE
+//     else if(value >= 97 && value <= 122){
+//       if(value <= 109) {
+//         result[i] = String.fromCharCode(value+13); 
+//       }
+//       if (value > 109){
+//          result[i] = String.fromCharCode(value -13); 
+//       } 
+//     }
+//     else{
+//     //everything else
+//       result[i] = inputs[i];
+//     }        
+//   } 
+//   return result.join();
+// }
+
+
+// var input = 'mnopqrstuvwxyzabcdeABC  s3 fghijklmnop';
+// var output = flipPairs(input);
+// console.log(output); 
+
+
+
+function transposeTwoStrings(array){
+//H W/n 
+//word A bigger than word b and opposite add spaces
+  let word1 = array[0];
+  let word2 = array[1];
   let result = [];
-  for (let i= 0; i<inputs.length;i++){
-    let value = inputs[i].charCodeAt(0);
-    //UPPER CASE
-    if(value >= 65 && value <= 90){
-      if(value <= 77) {
-        result[i] = String.fromCharCode(value+13); 
+  let lengthDiff = (word1.length - word2.length);
+
+    // if length diff is posative then add spaces
+  if(lengthDiff >0 ){
+    for (let a=0; a<lengthDiff; a++) {
+      word2 = word2.concat(' '); 
       }
-      if (value > 77){
-         result[i] = String.fromCharCode(value -13); 
+  }else if (lengthDiff<0){
+     for (let a=0; a>lengthDiff; a--)  {
+      word1 = word1.concat(' '); 
       }
-    }
-    //LOWER CASE
-    else if(value >= 97 && value <= 122){
-      if(value <= 109) {
-        result[i] = String.fromCharCode(value+13); 
-      }
-      if (value > 109){
-         result[i] = String.fromCharCode(value -13); 
-      } 
-    }
-    else{
-    //everything else
-      result[i] = inputs[i];
-    }        
-  } 
-  return result.join();
+  }
+  //console.log(lengthDiff + " " +word1 + word2);
+
+  for (let i = 0; i<word1.length; i++){
+    result.push(word1[i] + ' ' + word2[i] + '\n');
+  }
+
+  // if lenght diff is negative then add space to word2
+
+
+  //loop join
+ //console.log(result)
+
+ return result.join('');
 }
 
+transposeTwoStrings(['Hello ','World   ']);
 
-var input = 'mnopqrstuvwxyzabcdeABC  s3 fghijklmnop';
-var output = flipPairs(input);
-console.log(output); 
